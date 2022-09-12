94648560ad06bd43906dfde0481b77424c69d436
savio.fonseca <savio.fonseca@ucl.br>
2022-09-12 17:09:30 -0300

    initial commit

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+// import { reactive } from 'vue'
+
+// export default reactive({ flashMessage: '', event: null })
+
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: { user: 'Adam Jahr' },
+  mutations: {},
+  actions: {},
+  modules: {},
+})
